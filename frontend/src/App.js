5dc6e6343385ea2931648496346c8dc2b303e802
Mike Morris <mikemorris@users.noreply.github.com>
2019-04-15 14:17:47 -0400

    add header and footer copy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,18 @@ import './styles/styles.scss';
 const App = () => (
   <div className="App">
     <header className="App-header">
+      <h1>Who paid 99¢?</h1>
+      <p>An exclusive club reserved only for people who paid 99¢</p>
+      <p>Aren't you curious? Please don't tell.</p>
     </header>
 
     <MyEnhancedForm />
+
+    <footer>
+      <p>Your name will be public. Obviously. That's the point. Just use a fake one if you want.</p>
+      <p>Your receipt will be emailed to you.</p>
+      <cite>All sales are final. Please don't sue us. No copyright intended.</cite>
+    </footer>
   </div>
 );
 
