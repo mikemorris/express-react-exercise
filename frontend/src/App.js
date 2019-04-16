d5d5c137d2338047b6393520f9732f63d874297f
Mike Morris <mikemorris@users.noreply.github.com>
2019-04-16 11:53:24 -0400

    switch from withFormik to Formik render prop, inject onSubmit to handle state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,55 @@
-import React from 'react';
-import MyEnhancedForm from "./components/Form/Form";
+import React, { Component } from 'react';
+import MyForm from "./components/Form/Form";
 import './styles/styles.scss';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <h1>Who paid 99¢?</h1>
-      <p>An exclusive club reserved only for people who paid 99¢</p>
-      <p>Aren't you curious? Please don't tell.</p>
-    </header>
+class App extends Component {
+  state = {
+    submitted: false,
+    payments: []
+  }
 
-    <MyEnhancedForm />
+  onSubmit = async (values, { setSubmitting }) => {
+    try {
+      const res = await fetch("http://localhost:3001/payments", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
 
-    <footer>
-      <p>Your name will be public. Obviously. That's the point. Just use a fake one if you want.</p>
-      <p>Your receipt will be emailed to you.</p>
-      <cite>All sales are final. Please don't sue us. No copyright intended.</cite>
-    </footer>
-  </div>
-);
+      this.setState({ submitted: true });
+    } catch(err) {
+      // TODO: Handle error
+    }
+
+    setSubmitting(false);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Who paid 99¢?</h1>
+          <p>An exclusive club reserved only for people who paid 99¢</p>
+          <p>Aren't you curious? Please don't tell.</p>
+        </header>
+
+        { !this.state.submitted && (
+            <MyForm 
+              onSubmit={this.onSubmit}
+            />
+          )
+        }
+
+        <footer>
+          <p>Your name will be public. Obviously. That's the point. Just use a fake one if you want.</p>
+          <p>Your receipt will be emailed to you.</p>
+          <cite>All sales are final. Please don't sue us. No copyright intended.</cite>
+        </footer>
+      </div>
+    );
+  }
+}
 
 export default App;
--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -1,119 +1,100 @@
 import React from 'react';
 import TextInput from "./TextInput";
-import { withFormik } from 'formik';
+import { Formik } from 'formik';
 
 const MyForm = ({
-  values,
-  touched,
-  errors,
-  isSubmitting,
-  handleChange,
-  handleBlur,
-  handleSubmit,
-  handleReset,
-  dirty,
+  onSubmit,
 }) => (
-  <form onSubmit={handleSubmit}>
-    <TextInput
-      id="first_name"
-      label="First Name"
-      placeholder="Margaret"
-      values={values}
-      touched={touched}
-      errors={errors}
-      handleChange={handleChange}
-      handleBlur={handleBlur}
-    />
+  <Formik
+    onSubmit={onSubmit}
+    validate={values => {
+      let errors = {};
+      if (!values.email) {
+        errors.email = 'Required';
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(
+          values.email
+        )
+      ) {
+        errors.email = 'Invalid email address';
+      }
+      return errors;
+    }}
+    render={({
+      values,
+      touched,
+      errors,
+      isSubmitting,
+      handleChange,
+      handleBlur,
+      handleSubmit,
+      handleReset,
+      dirty,
+    }) => (
+      <form onSubmit={handleSubmit}>
+        <TextInput
+          id="first_name"
+          label="First Name"
+          placeholder="Margaret"
+          values={values}
+          touched={touched}
+          errors={errors}
+          handleChange={handleChange}
+          handleBlur={handleBlur}
+        />
 
-    <TextInput
-      id="last_name"
-      label="Last Name"
-      placeholder="Hamilton"
-      values={values}
-      touched={touched}
-      errors={errors}
-      handleChange={handleChange}
-      handleBlur={handleBlur}
-    />
+        <TextInput
+          id="last_name"
+          label="Last Name"
+          placeholder="Hamilton"
+          values={values}
+          touched={touched}
+          errors={errors}
+          handleChange={handleChange}
+          handleBlur={handleBlur}
+        />
 
-    <p>
-      The following information needs to be real for your payment to be processed.
-    </p>
+        <p>
+          The following information needs to be real for your payment to be processed.
+        </p>
 
-    <TextInput
-      id="email"
-      label="Email"
-      placeholder="margaret@nasa.gov"
-      values={values}
-      touched={touched}
-      errors={errors}
-      handleChange={handleChange}
-      handleBlur={handleBlur}
-    />
-    
-    <label htmlFor="payment" style={{ display: 'block' }}>
-      Saved Payment
-    </label>
-    <span
-      className="icon--visa-card"
-      style={{
-        "padding-left": "34px"
-      }}
-    >
-      <input
-        id="payment"
-        placeholder="**** **** **** 4242"
-        type="text"
-        value={values["payment"]}
-        disabled
-        onChange={handleChange}
-        onBlur={handleBlur}
-      />
-    </span>
+        <TextInput
+          id="email"
+          label="Email"
+          placeholder="margaret@nasa.gov"
+          values={values}
+          touched={touched}
+          errors={errors}
+          handleChange={handleChange}
+          handleBlur={handleBlur}
+        />
+        
+        <label htmlFor="payment" style={{ display: 'block' }}>
+          Saved Payment
+        </label>
+        <span
+          className="icon--visa-card"
+          style={{
+            "padding-left": "34px"
+          }}
+        >
+          <input
+            id="payment"
+            placeholder="**** **** **** 4242"
+            type="text"
+            value={values["payment"]}
+            disabled
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+        </span>
 
-    <button type="submit" disabled={isSubmitting}>
-      See who paid 99¢
-    </button>
-  </form>
+        <button type="submit" disabled={isSubmitting}>
+          See who paid 99¢
+        </button>
+      </form>
+    )}
+  />
 );
 
-const MyEnhancedForm = withFormik({
-  mapPropsToValues: () => ({ email: '' }),
-
-  // Custom sync validation
-  validate: values => {
-    let errors = {};
-    if (!values.email) {
-      errors.email = 'Required';
-    } else if (
-      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(
-        values.email
-      )
-    ) {
-      errors.email = 'Invalid email address';
-    }
-    return errors;
-  },
-
-  handleSubmit: async (values, { setSubmitting }) => {
-    const res = await fetch("http://localhost:3001/payments", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(values),
-    });
-
-    setSubmitting(false);
-
-    if (res.status === 200) {
-      // TODO: Set success state
-    } else {
-      // Handle error
-    }
-  },
-
-  displayName: 'BasicForm', // helps with React DevTools
-})(MyForm);
-
-export default MyEnhancedForm;
+export default MyForm;
